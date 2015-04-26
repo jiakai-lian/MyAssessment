ab7237bb6189c0df82cfd6aa6887fb6142e34fd5
jiakailian <jiakai.lian@gmail.com>
2015-04-26 23:48:08 +1000

    create the project and configure storyboard for view 1

---
+++ MyAssessment/MyAssessment/Controller/View1ViewController.h
@@ -0,0 +1,13 @@
+//
+//  View1ViewController.h
+//  MyAssessment
+//
+//  Created by jiakai lian on 26/04/2015.
+//  Copyright (c) 2015 jiakai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface View1ViewController : UIViewController
+
+@end
